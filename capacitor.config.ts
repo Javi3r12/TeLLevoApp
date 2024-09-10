a256ecb2a1b370b5c6686ed6cddd50ae99aa9509
CETECOM <cetecom_conce@duoc.cl>
2024-09-10 10:48:48 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TeLLevoApp',
+  webDir: 'www'
+};
+
+export default config;
