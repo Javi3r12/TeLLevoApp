a4c320b1c9328bbf916336e0e173404cfd167a1a
CETECOM <cetecom_conce@duoc.cl>
2024-11-04 15:01:25 -0300

    gestion de vehiculos por propiedad del usuario

--- src/app/pages/gestion-vehiculos/gestion-vehiculos.page.ts
+++ src/app/pages/gestion-vehiculos/gestion-vehiculos.page.ts
@@ -3,6 +3,7 @@ import { FirebaseService } from 'src/app/services/firebase.service';
 import { Vehiculo } from 'src/app/interfaces/vehiculo.model';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { sesionService } from 'src/app/services/sesion.service';
 
 @Component({
   selector: 'app-gestion-vehiculos',
@@ -10,13 +11,17 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./gestion-vehiculos.page.scss'],
 })
 export class GestionVehiculosPage implements OnInit {
-  
+  userId: any;
   vehiculos : Vehiculo[] = [];
 
-  constructor(private firebase: FirebaseService, private router: Router, private alertctrl: AlertController) { }
+  constructor(private firebase: FirebaseService, private router: Router,
+    private alertctrl: AlertController, private sesion: sesionService) 
+    { 
+      this.userId = this.sesion.getUser()?.id;
+    }
 
   ngOnInit() {
-    this.cargarvehiculos()
+    this.cargarVehiculos()
   }
 
   editarVehiculo(vehiculo: Vehiculo){
@@ -39,6 +44,29 @@ export class GestionVehiculosPage implements OnInit {
         this.vehiculos = data
       }
     })
+  }
+
+  cargarVehiculos() {
+    this.firebase.getCollectionChanges<{ id_user: string, id: string }>('vehiculos')
+      .subscribe(viajeIns => {
+        if (viajeIns) {
+          console.log('viajesIns =>',viajeIns)
+
+          const viajesUsuario = viajeIns.filter(v => v.id_user === this.userId);
+          console.log('viajesUsuario', viajesUsuario)
+
+          const viajeIds = viajesUsuario.map(v => v.id);
+          console.log('viajesIds =>',viajeIds)
+
+          this.firebase.getCollectionChanges<Vehiculo>('vehiculos').subscribe(data => {
+            if (data) {
+              console.log(data)
+              this.vehiculos = data.filter(vehiculo => viajeIds.includes(vehiculo.id));
+              console.log(this.vehiculos)
+            }
+          })
+        }
+      });
   }
 
   async alerta(vehiculo: Vehiculo){
