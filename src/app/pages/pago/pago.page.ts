4c3295b18a0487a219aa706d0f0da43992b34249
CETECOM <cetecom_conce@duoc.cl>
2024-11-07 10:15:24 -0300

    pago falta por correguir

--- src/app/pages/pago/pago.page.ts
+++ src/app/pages/pago/pago.page.ts
@@ -3,6 +3,7 @@ import { Viaje } from 'src/app/interfaces/viaje.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { FirebaseService } from 'src/app/services/firebase.service';
+import {  NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-pago',
@@ -11,7 +12,19 @@ import { FirebaseService } from 'src/app/services/firebase.service';
 })
 export class PagoPage implements OnInit {
 
-  viaje: Viaje | null = null;
+  viaje: Viaje = {
+    id: '' ,
+    destino: '',
+    asientos: 0,
+    vehiculo: '',
+    descripcion: '',
+    precio: 0,
+    activo: true, 
+    id_user: ''
+  };
+  asientos: number | undefined ;
+  uno = 1; 
+  pago: string | undefined ; 
 
   constructor(private alertctrl:AlertController, private router: Router, private route: ActivatedRoute ,private firebase: FirebaseService ) { }
 
@@ -24,8 +37,19 @@ export class PagoPage implements OnInit {
     }
   }
 
-  cancelarPago(){
-    this.alerta()
+  cancelarPago(form: NgForm){
+    if(form.valid){
+      console.log(this.viaje.asientos)
+      this.asientos = this.viaje.asientos;
+      this.asientos = (this.asientos - 1) ;
+      this.viaje.asientos = this.asientos;
+      console.log(this.asientos)
+      console.log(this.viaje.asientos)
+      this.alerta()
+    } if(form.invalid) {
+      console.log('error de pago')
+      this.alertaPago()
+    }
   }
 
   cargarViaje(id: string) {
@@ -55,4 +79,16 @@ export class PagoPage implements OnInit {
     await alert.present();
   }
 
+  async alertaPago(){
+    console.log("Alerta desde controller");
+    const alert = await this.alertctrl.create({
+      header: 'Debe seleccionar un metodo',
+      message: 'Debe completar el metodo de pago para continuar con el pago',
+      buttons: [
+        {text: 'aceptar'}
+      ],
+    });
+
+    await alert.present();
+  }
 }
